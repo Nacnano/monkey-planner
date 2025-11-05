d85c434dec9444e23633ff83e6e373c47cb8a45e
Chotpisit Adunsehawat <chotpisit080916c@gmail.com>
2025-11-06 00:54:32 +0700

    fix: update initial data

--- src/data/initialData.ts
+++ src/data/initialData.ts
@@ -1,25 +1,25 @@
 import type { Course } from "../types";
 
 export const initialCourses: Course[] = [
-  {
-    id: crypto.randomUUID(),
-    name: "ปรับพื้นฐาน TGAT",
-    sheetCount: 20,
-    examName: "TGAT / TPAT2-5",
-    examDate: "2025-12-13",
-  },
-  {
-    id: crypto.randomUUID(),
-    name: "ตะลุยโจทย์ TPAT1",
-    sheetCount: 30,
-    examName: "TPAT1 (กสพท)",
-    examDate: "2026-02-14",
-  },
-  {
-    id: crypto.randomUUID(),
-    name: "เก็บเนื้อหา A-Level",
-    sheetCount: 40,
-    examName: "สอบ A-Level",
-    examDate: "2026-03-14",
-  },
+  // {
+  //   id: crypto.randomUUID(),
+  //   name: "ปรับพื้นฐาน TGAT",
+  //   sheetCount: 20,
+  //   examName: "TGAT / TPAT2-5",
+  //   examDate: "2025-12-13",
+  // },
+  // {
+  //   id: crypto.randomUUID(),
+  //   name: "ตะลุยโจทย์ TPAT1",
+  //   sheetCount: 30,
+  //   examName: "TPAT1 (กสพท)",
+  //   examDate: "2026-02-14",
+  // },
+  // {
+  //   id: crypto.randomUUID(),
+  //   name: "เก็บเนื้อหา A-Level",
+  //   sheetCount: 40,
+  //   examName: "สอบ A-Level",
+  //   examDate: "2026-03-14",
+  // },
 ];
